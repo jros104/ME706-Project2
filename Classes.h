44a1bcb540d05fb0681704ad988eadcfd1fe7388
samuel-reedy <64679797+samuel-reedy@users.noreply.github.com>
2023-05-11 13:39:45 +1200

    meant to include this in last commit

---
+++ Classes.h
@@ -0,0 +1,14 @@
+#ifndef Classes_H
+#define Classes_H
+
+#include <Sensor.h>
+#include <IRSensor.h>
+#include <Sonar.h>
+#include <Fan.h>
+#include <Phototransistor.h>
+#include <Timer.h>
+#include <FireLights.h>
+#include <Defines.h>
+#include <Kalman.h>
+
+#endif
