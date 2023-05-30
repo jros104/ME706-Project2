0970e18e4a8c2b54c2ba23a1f774e11cf23a18b2
samuel-reedy <64679797+samuel-reedy@users.noreply.github.com>
2023-05-30 14:46:31 +1200

    updated defines

--- Classes/src/Defines.h
+++ Classes/src/Defines.h
@@ -10,12 +10,12 @@
 #define NO_READ_GYRO  //Uncomment if GYRO is not attached.
 #define NO_HC-SR04 //Uncomment of HC-SR04 ultrasonic ranging sensor is not attached.
 //#define NO_BATTERY_V_OK //Uncomment of BATTERY_V_OK if you do not care about battery damage.
-#define USE_KALMAN false
+#define USE_KALMAN true
 
 #define PHOTO_L_SHORT_PIN A5
 #define PHOTO_R_SHORT_PIN A4
-#define PHOTO_R_LONG_PIN A7
-#define PHOTO_L_LONG_PIN A6
+#define PHOTO_R_LONG_PIN A6
+#define PHOTO_L_LONG_PIN A7
 
 #define IR_L_LONG_PIN A10
 #define IR_R_LONG_PIN A11
@@ -34,7 +34,7 @@
 // Fire Detection
 #define FIRE_DETECTION_ROTATION_SPEED 0.1
 #define FIRE_ROTATION_SPEED 0.1
-#define FIRE_DETECTION_TOLERANCE 50
+#define FIRE_DETECTION_TOLERANCE 15
 
 // Approach Fire
 #define APPROACH_FIRE_DIST 25
@@ -42,13 +42,14 @@
 #define BASE_SPEED 2.5
 
 // Obstacle Avoidance
-#define OBSTIK_DIST 7
-#define OBSTIK_DIST_EXTRA 3
-#define OBSTIK_MIN_SPACE 25
+#define OBSTIK_DIST_SONAR 7
+#define OBSTIK_DIST_IR 9
+#define OBSTIK_MIN_SPACE 32
+#define OBSTIK_SIDE_MIN 15
 #define OBSTIK_CLEAR 20
 #define AVOID_SPEED BASE_SPEED
-#define AVOID_DELAY_SIDEWAYS 200
-#define AVOID_DELAY_FORWARDS 1100
-#define REVERSE_TIME 300
+#define AVOID_DELAY_SIDEWAYS 150
+#define AVOID_DELAY_FORWARDS 1300
+#define REVERSE_TIME 150
 
 #endif
